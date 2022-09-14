6399dc6cb2272fe7521a3a228aabee9c3453cf8c
kimbank <kimeunhang@inha.edu>
2022-09-14 22:54:45 +0900

    Home. Ffixes #1

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom"
 import { useState } from "react";
 
 import Home from '../views/Home/Home';
+import Video from '../views/Video/Video';
 
 const App = function () {
 	// const [name, setName] = useState();
@@ -12,6 +13,9 @@ const App = function () {
 				<Switch>
 					<Route exact>
 						<Home />
+					</Route>
+					<Route path="/video">
+						<Video />
 					</Route>
 
 					<Redirect to="/" />
