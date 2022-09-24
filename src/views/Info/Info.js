5c6431369314de8fd3f70fd6c81c9bd770c60325
kimbank <kimeunhang@inha.edu>
2022-09-24 20:00:28 +0900

    fix: info #1

--- src/views/Info/Info.js
+++ src/views/Info/Info.js
@@ -6,7 +6,10 @@ import "./Info.css"
 import "../../../resources/css/style.css"
 
 import BG from '../../../resources/home_assets/webOS_BG.png';
+import profile_bg from '../../../resources/home_assets/circle_dots.png';
 import homeBtn from '../../../resources/home_assets/home_button_eng.png';
+
+import sample_profile_image from '../../../resources/prototype.png';
 
 const Info = () => {
     const history = useHistory();
@@ -17,8 +20,22 @@ const Info = () => {
 
     return(
         <div className="wrap-info">
-            <div className="info-frame" style={{background: BG}}>
-                
+            <div className="info-frame">
+                <div className="info-top">
+                    <div className="bio-info">
+                        마루 27개월 5kg <br/>
+                        포메라니안 (암컷)
+                    </div>
+                    <div className="title-info">
+                        Health 리포트 <br/>
+                        <div style={{backgroundColor: 'red',width:'100%',height:'10px'}}></div>
+                    </div>
+                </div>
+                <div className="info-prof">
+                    <img className="prof" src={sample_profile_image} />
+                    <img className="prof_bg" src={profile_bg} />
+                </div>
+                <img className="bg" src={BG} />
             </div>
             <div className="home-section">
                 <img src={homeBtn} width="100px" onClick={onGotoHome} />
